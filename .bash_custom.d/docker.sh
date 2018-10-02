d9efaed0ca62b204db4914978c09e1ec15fe2d2a
Julien Kassar <github@kassisol.com>
2018-10-02 10:15:51 -0400

    Remove docker functions

--- .bash_custom.d/docker.sh
+++
@@ -1,4 +0,0 @@
-# docker aliases
-alias dockercleanup='docker ps --filter "status=exited" -q | xargs docker rm {} && docker images --filter "dangling=true" -q | xargs docker rmi {}'
-
-alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
