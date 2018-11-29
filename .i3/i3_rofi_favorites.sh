766a8393cf38c8494ccffbcc08f9a70f9b584610
Julien Kassar <github@kassisol.com>
2018-11-29 11:24:30 -0500

    Update i3_rofi_favorites.sh script

--- .i3/i3_rofi_favorites.sh
+++ .i3/i3_rofi_favorites.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 list_favorites() {
-	echo -e "Thunar\nFirefox\nChromium\nThunderbird\nKeePassXC\nLibreOffice\nGedit"
+	echo -e "Thunar\nFirefox\nChromium\nThunderbird\nKeePassXC\nLibreOffice\nGedit\nTranslate\nDicoFR\nConjugaisonFR"
 }
 
 FAVORITE=$(list_favorites | rofi -dmenu -p "Select favorite:")
@@ -27,6 +27,15 @@ LibreOffice)
 	;;
 Gedit)
 	exec gedit
+	;;
+Translate)
+	exec firefox --new-tab https://deepl.com
+	;;
+DicoFR)
+	exec firefox --new-tab https://fr.wiktionary.org
+	;;
+ConjugaisonFR)
+	exec firefox --new-tab http://www.conjugaison.com
 	;;
 *)
 	exit 1
